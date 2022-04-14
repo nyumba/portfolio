e756c1098925ad0e3829fb8a6fc0a4f5713d4fc7
unknown <vincentnyumba@gmail.com>
2022-04-14 18:50:05 +0300

    Innitial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section className="p-8">
+          <div className="text-8xl text-orange-500 font-playball">
+            Vincent Nyumba
+          </div>
+          <br />
+          <div className="font-lato text-orange-400 uppercase text-3xl">
+            Full-Stack Developer & UI/UX Designer
+          </div>
+        </section>
+        <section>
+          <div className="font-lato text-3xl tracking-wider">
+            Full-stack developer who cares deeply about user experience.
+            <br />
+            Serious passion for full-stack developent and new technologies.
+          </div>
+        </section>
       </header>
     </div>
   );
